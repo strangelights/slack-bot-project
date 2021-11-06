17c3433a31646a4827e4fce07230856bf3b2d17b
Andre Pato <anpato1994@gmail.com>
2021-11-06 15:20:31 -0400

    update starter code

--- scripts/bot.js
+++ scripts/bot.js
@@ -1,8 +1,1 @@
-module.exports = (bot) => {
-  bot.hear(/Bot/, function (res) {
-    return res.send('Hello From bot')
-  })
-  bot.hear(/What day is today?/, function (res) {
-    return res.send(new Date().toDateString())
-  })
-}
+module.exports = function (bot) {}
